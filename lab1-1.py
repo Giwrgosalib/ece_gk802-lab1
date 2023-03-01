e0100f6623334deae5e56471a1740f81b916dd3a
Γεωργιος Εμμανουηλ Αλιμπαλης <up1070520@upnet.gr>
2023-03-01 21:25:17 +0200

    Change for last Requirements

--- lab1-1.py
+++ lab1-1.py
@@ -1,5 +1,20 @@
 import requests
+from datetime import datetime;
 x=input("Enter Desired URL:")
 r=requests.get(x)
 for i in r.headers:
     print(i)
+print("Server:"+r.headers['Server'])
+if('Set-Cookie' in r.headers):
+    print("The URL has Cookies\n")
+    for c in r.cookies:
+        if c.expires==None:
+            print(c.name)
+            print("No expiration date")
+        else:
+            expdate=float(c.expires)
+            nowdate=datetime.now().timestamp()
+            print(c.name +" \nTime left Until Expiration:"+ str(expdate-nowdate)+" Seconds left")
+else:
+    print("The URL has no Cookies")
+
